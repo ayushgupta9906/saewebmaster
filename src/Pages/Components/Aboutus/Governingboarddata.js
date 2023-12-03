820002c11b2e2b85f1d537b2a3ed1aef09fe93ce
saezhcetamu <sae@zhcet.ac.in>
2023-12-03 14:20:45 +0530

    first commit

---
+++ src/Pages/Components/Aboutus/Governingboarddata.js
@@ -0,0 +1,119 @@
+export default [
+    {
+        Name:"Anant Agarwal",
+        Post:"Chairperson",
+        LinkedIn:"https://www.linkedin.com/in/anant-agarwal-3b3550212",
+        Insta:"",
+        Email:"anant.olf@gmail.com",
+        Img:"Anant.png"
+    },
+
+    {
+        Name:"Shadab Khan",
+        Post:"Vice-Chairperson",
+        LinkedIn:"https://www.linkedin.com/in/shadab-khan-4b837a217",
+        Insta:"https://www.instagram.com/shadab.khn_/",
+        Email:"shadabkhan.amu10@gmail.com",
+        Img:"Shadab.png"
+    },
+
+    {
+        Name:"Ritik Prabhat",
+        Post:"Secretary",
+        LinkedIn:"https://www.linkedin.com/in/ritik-prabhat-3357b420b?originalSubdomain=in",
+        Insta:"https://www.instagram.com/Ritik_prabhat_/",
+        Email:"ritikprabhat19@gmail.com",
+        Img:"ritik_prabhat.jpg"
+    },
+
+    {
+        Name:"Nirmanyu Goel",
+        Post:"Secretary",
+        LinkedIn:"",
+        Insta:"https://www.instagram.com/_nirmanyu_goel/",
+        Email:"nirmanyugoel@gmail.com",
+        Img:"Nirmanyu_Goel.jpg"
+    },
+
+    {
+        Name:"Nousheen Shahabi",
+        Post:"Membership & Recruitment Coordinator",
+        LinkedIn:"",
+        Insta:"",
+        Email:"shahabinousheen@gmail.com",
+        Img:"nousheen_shahabi.jpg"
+    },
+
+    {
+        Name:"Mohd Munazir Mairaj",
+        Post:"Treasurer",
+        LinkedIn:"https://www.linkedin.com/in/mohd-munazir-mairaj-1432761b1",
+        Insta:"https://www.instagram.com/i__am___munazir/",
+        Email:"munazirmairaj8976@gmail.com",
+        Img:"mohd_munazir.jpg"
+    },
+
+    {
+        Name:"Abdur Rehman Alvi",
+        Post:"Event and workshop coordinator ",
+        LinkedIn:"https://www.linkedin.com/in/abdur-rehman-alvi-0a2a63205",
+        Insta:"https://www.instagram.com/abdurrahman_alvi_03/",
+        Email:"abdurcoc74@gmail.com",
+        Img:"WhatsApp Image 2023-08-23 at 21.58.34.jpg"
+    },
+
+    {
+        Name:"Juaria Fatima",
+        Post:"Coordinator - Public Relations & Outreach",
+        LinkedIn:"www.linkedin.com/in/ juaria-fatima",
+        Insta:"",
+        Email:"fatimajuaria@gmail.com",
+        Img:"juaria_fatima.jpg"
+    },
+
+    {
+        Name:"Hasnain Abbas Jafri",
+        Post:"Editorial-Head",
+        LinkedIn:"",
+        Insta:"https://www.instagram.com/jafri012/",
+        Email:"jafrihasnain397@gmail.com",
+        Img:"hasnain_jafri.jpg"
+    },
+
+    {
+        Name:"Mohd Yawar",
+        Post:"Coordinator - Industry Interaction",
+        LinkedIn:"http://www.linkedin.com/in/mohdyawar",
+        Insta:"https://www.instagram.com/yaaawarrrrr/",
+        Email:"mohd.yawar.125@gmail.com",
+        Img:"mohd_yawar.JPG"
+    },
+
+    {
+        Name:"Shahwaiz Islam",
+        Post:"Coordinator -Alumini Relations and Buisness",
+        LinkedIn:"https://www.linkedin.com/in/shahwaiz-islam",
+        Insta:"https://www.instagram.com/shahwaiz1404/",
+        Email:"shahwaizislam1404@gmail.com",
+        Img:"shahwaiz_islam2.jpg"
+    },
+    
+    {
+        Name:"Ayush Gupta",
+        Post:"Web Developer",
+        LinkedIn:"https://in.linkedin.com/in/ayush-gupta-3a93621b6",
+        Insta:"https://www.instagram.com/laserx_1729/",
+        Email:"ayush150152@gmail.com",
+        Img:"ayush_gupta_gif.gif"
+    },
+
+    {
+        Name:"Mohd Anas Maroof",
+        Post:"Web Developer",
+        LinkedIn:"",
+        Insta:"https://www.instagram.com/anas_maroof_/",
+        Email:"ayanlove3062012@gmail.com",
+        Img:"anas_maroof2.jpeg"
+    }
+
+]
